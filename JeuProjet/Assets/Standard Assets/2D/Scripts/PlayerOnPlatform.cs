4d8ac94960d657802f66d316a59c42e61bc6072f
Thomas <legristhomaspro@gmail.com>
2019-06-14 19:39:43 +0200

    Plateforme mouvante, le joueur suis la plateforme lorsqu'il est dessus

---
+++ JeuProjet/Assets/Standard Assets/2D/Scripts/PlayerOnPlatform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOnPlatform : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = this.gameObject.transform;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+        }
+    }
+}
