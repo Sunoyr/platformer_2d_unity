0c1ff8b1d70006f9c610a06cd7882a65728d85ce
Artosis <thomas.displaire@outlook.fr>
2019-06-14 19:28:18 +0200

    Ajout scie circu

---
+++ JeuProjet/Assets/Standard Assets/2D/Scripts/ScieScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ScieScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("ofjhgpoedr");
+            SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+        }
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0, 0, 5);
+    }
+}
