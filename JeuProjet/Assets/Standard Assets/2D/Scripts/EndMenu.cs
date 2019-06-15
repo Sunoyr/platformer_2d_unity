48fdf66a4a516e6c3cabeb581f653798768cca46
Thomas <legristhomaspro@gmail.com>
2019-06-15 20:48:11 +0200

    Création menu pause + script & création menu fin + script

---
+++ JeuProjet/Assets/Standard Assets/2D/Scripts/EndMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndMenu : MonoBehaviour
+{
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
---
+++ JeuProjet/Assets/Standard Assets/2D/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    bool showGUI = false;
+    public GameObject canvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            showGUI = !showGUI;
+        }
+
+        if (showGUI)
+        {
+            canvas.SetActive(true);
+            Time.timeScale = 0;
+        } else
+        {
+            canvas.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    void OnLevelWasLoaded()
+    {
+        canvas = GameObject.Find("Canvas Pause");
+    }
+
+    public void LEAVEGAME()
+    {
+        Application.Quit();
+    }
+
+    public void LOADMAINMENU()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
