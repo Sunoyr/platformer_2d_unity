4d059c40bfa4fe8b21def3d42a2df3fd6fcdd2f0
Thomas <legristhomaspro@gmail.com>
2019-06-15 18:33:12 +0200

    Menu Principale Fonctionnel

---
+++ JeuProjet/Assets/Standard Assets/2D/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void STARTGAME()
+    {
+        SceneManager.LoadScene("Monde1_1");
+    }
+
+    public void LEAVEGAME()
+    {
+        Application.Quit();
+    }
+}
