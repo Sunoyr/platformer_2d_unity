8485f868c22f6c1f727d047fd44df4d75f9022b7
Artosis <thomas.displaire@outlook.fr>
2019-06-15 16:27:08 +0200

    Ajout score entre les scènes + reset niveau 1

--- JeuProjet/Assets/Standard Assets/2D/Scripts/TeleporterScript.cs
+++ JeuProjet/Assets/Standard Assets/2D/Scripts/TeleporterScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class TeleporterScript : MonoBehaviour
@@ -9,11 +10,29 @@ public class TeleporterScript : MonoBehaviour
 
     public string NomLevel;
 
+    private void Start()
+    {
+        if(SceneManager.GetActiveScene().name == "Monde1_1")
+        {
+            PlayerPrefs.SetInt("Score", 0);
+        }
+
+        Debug.Log(PlayerPrefs.GetInt("Score"));
+        var text = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>().text = PlayerPrefs.GetInt("Score").ToString();
+
+
+    }
+
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
         {
+            var text = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
+            var oldscore = int.Parse(text.text);
+            PlayerPrefs.SetInt("Score", oldscore);
+
             SceneManager.LoadScene(NomLevel);
         }
     }
 }
+
