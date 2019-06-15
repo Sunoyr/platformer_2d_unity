505cc113935c17d54df73f3a317086e57c87af00
Thomas <legristhomaspro@gmail.com>
2019-06-15 16:00:12 +0200

    Début UI

--- JeuProjet/Assets/Standard Assets/2D/Scripts/Piècette.cs
+++ JeuProjet/Assets/Standard Assets/2D/Scripts/Piècette.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Piècette : MonoBehaviour
 {
+    bool pris = true;
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("Player"))
+        if (col.CompareTag("Player") && pris)
         {
-
             Destroy(gameObject);
+            var text = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
+            var oldscore = int.Parse(text.text);
+            text.text = (oldscore + 1).ToString();
+            pris = false;
         }
     }
 }
